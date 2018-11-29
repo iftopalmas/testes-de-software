2b903111e01c3264e1e9b69c73ed47d90c0d7f1f
Manoel Campos <manoelcampos@gmail.com>
2018-11-29 16:26:01 -0300

    Adiciona documentação de pacotes e atualiza README

---
+++ projects/02-sistema-bancario/src/main/java/com/sistemabancario/dao/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote contendo classes que implementam o Padrão de Projetos Data Access Objects (DAO).
+ * Tais classes são responsáveis por fazer o acesso ao banco de dados.
+ *
+ * @author Manoel Campos da Silva Filho
+ */
+package com.sistemabancario.dao;
