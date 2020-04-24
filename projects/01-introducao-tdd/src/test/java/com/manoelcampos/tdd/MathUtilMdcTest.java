93f55372b537c919f035e193974e0d334a8c9cf4
Manoel Campos <manoelcampos@gmail.com>
2020-04-24 15:12:47 -0300

    Atualiza projeto MDC

--- projects/01-introducao-tdd/src/test/java/com/manoelcampos/tdd/MathUtilMdcTest.java
+++ projects/01-introducao-tdd/src/test/java/com/manoelcampos/tdd/MathUtilMdcTest.java
@@ -15,19 +15,19 @@ import static org.junit.jupiter.api.Assertions.*;
  * 
  * @author Manoel Campos da Silva Filho <http://github.com/manoelcampos>
  */
-public class MathUtilMdcTest {
+class MathUtilMdcTest {
 
     /**
      * Propriedade 1: Se b > 0 é um divisor de a, então mdc(a, b) = b
      * Verifica b positivo.
      */
     @Test
-    public void testMdc_P1_Quando_B_Positivo() {
-        int a = 6;
-        int b = 2;
-        int expResult = b;
-        int result = MathUtil.mdc(a, b);
-        assertEquals(expResult, result, 0.0); 
+    void testMdc_P1_Quando_B_Positivo() {
+        final int a = 6;
+        final int b = 2;
+        final int esperado = b;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido, 0.0); 
     }
 
     /**
@@ -35,12 +35,12 @@ public class MathUtilMdcTest {
      * Verifica b negativo.
      */
     @Test
-    public void testMdc_P1_Quando_B_Negativo() {
-        int a = 6;
-        int b = -2;
-        int expResult = 2;
-        int result = MathUtil.mdc(a, b);
-        assertEquals(expResult, result, 0.0); 
+    void testMdc_P1_Quando_B_Negativo() {
+        final int a = 6;
+        final int b = -2;
+        final int esperado = 2;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido, 0.0); 
     }
 
     /**
@@ -50,12 +50,12 @@ public class MathUtilMdcTest {
      * Este teste verifica um a positivo.
      */
     @Test
-    public void testMdc_P3_Quando_A_Positivo() {
-        int a = 6;
-        int b = 0;
-        int expResult = 6;
-        int result = MathUtil.mdc(a, b);
-        assertEquals(expResult, result, 0.0);
+    void testMdc_P3_Quando_A_Positivo() {
+        final int a = 6;
+        final int b = 0;
+        final int esperado = 6;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido, 0.0);
     }
     
     /**
@@ -65,12 +65,12 @@ public class MathUtilMdcTest {
      * Este teste verifica um a negativo.
      */
     @Test
-    public void testMdc_P3_Quando_A_Negativo() {
-        int a = -6;
-        int b = 0;
-        int expResult = 6;
-        int result = MathUtil.mdc(a, b);
-        assertEquals(expResult, result, 0.0);
+    void testMdc_P3_Quando_A_Negativo() {
+        final int a = -6;
+        final int b = 0;
+        final int esperado = 6;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido, 0.0);
     }
 
     /**
@@ -78,25 +78,25 @@ public class MathUtilMdcTest {
      * mdc(m*a, m*b) = m * mdc(a, b)
      */
     @Test
-    public void testMdc_P4() {
-        int m = 2;
-        int a = 6;
-        int b = 0;
-        int expResult = m*MathUtil.mdc(a, b);
-        int result = MathUtil.mdc(m*a, m*b);
-        assertEquals(expResult, result, 0.0);
+    void testMdc_P4() {
+        final int m = 2;
+        final int a = 6;
+        final int b = 0;
+        final int esperado = m*MathUtil.mdc(a, b);
+        final int obtido = MathUtil.mdc(m*a, m*b);
+        assertEquals(esperado, obtido, 0.0);
     }
     
     /**
      * Testa se o MDC é 1 quando a não é divisível por b.
      */
     @Test
-    public void testMdc_ResultadoIgual1() {
-        int a = 9;
-        int b = 2;
-        int expResult = 1;
-        int result = MathUtil.mdc(a, b);
-        assertEquals(expResult, result, 0.0);
+    void testMdc_obtidoadoIgual1() {
+        final int a = 9;
+        final int b = 2;
+        final int esperado = 1;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido, 0.0);
     }
 
     /**
@@ -109,84 +109,72 @@ public class MathUtilMdcTest {
      * que é a forma comum de calcular o MDC que aprendemos na escola.
      */
     @Test
-    public void testMdc_P5() {
-        int a = 9;
-        int b = 2;
-        int c = 5;
-        int expResult = MathUtil.mdc(a,c) * MathUtil.mdc(b,c);
-        int result = MathUtil.mdc(a*b, c);
-        assertEquals(expResult, result, 0.0);
+    void testMdc_P5() {
+        final int a = 9;
+        final int b = 2;
+        final int c = 5;
+        final int esperado = MathUtil.mdc(a,c) * MathUtil.mdc(b,c);
+        final int obtido = MathUtil.mdc(a*b, c);
+        assertEquals(esperado, obtido, 0.0);
     }
 
     /**
      * Propriedade 6: mdc(a, b) = mdc(b, a).
      */
     @Test
-    public void testMdc_P6() {
-        //Matriz onde cada linha representa um par de valores a,b para serem testados
-        int matrizValores[][] = {{9,2}, {6,2}, {6,0}, {-6,0}};
-        int expResult;
-        int result;
-        String msg;
-        for (int[] valores : matrizValores) {
-            expResult = MathUtil.mdc(valores[1], valores[0]);
-            result = MathUtil.mdc(valores[0], valores[1]);
-            msg = String.format("a: %d b: %d", valores[0], valores[1]);
-            assertEquals(expResult, result, msg);
-        }
+    void testMdc_P6() {
+        final int a = 9;
+        final int b = 2;
+        final int esperado = MathUtil.mdc(a, b);
+        final int obtido = MathUtil.mdc(b, a);
+        assertEquals(esperado, obtido);
     }
     
     /**
-     * Propriedade 7: mdc(-a, b) = mdc(a, -b) = mdc(-a, -b) = mdc(a, b)
+     * Parte da Propriedade 7: mdc(-a, b) = mdc(a, -b)
      */
     @Test
-    public void testMdc_P7() {
-        int a = 8;
-        int b = 2;
-        int expResult = MathUtil.mdc(a, -b);
-        int result = MathUtil.mdc(-a, b);
-        assertEquals(expResult, result);
-        
-        expResult = MathUtil.mdc(-a, -b);
-        assertEquals(expResult, result);
-
-        expResult = MathUtil.mdc(a, b);
-        assertEquals(expResult, result);
+    void testMdc_P7() {
+        final int a = 8;
+        final int b = 2;
+        final int esperado = MathUtil.mdc(-a, b);
+        final int obtido = MathUtil.mdc(a, -b);
+        assertEquals(esperado, obtido);
     }   
     
     /**
      * Propriedade 8: Se a é um número inteiro positivo, então mdc(a,a) = a
      */
     @Test
-    public void testMdc_P8() {
-        int a = 9;
-        int expResult = a;
-        int result = MathUtil.mdc(a, a);
-        assertEquals(expResult, result);
+    void testMdc_P8() {
+        final int a = 9;
+        final int esperado = a;
+        final int obtido = MathUtil.mdc(a, a);
+        assertEquals(esperado, obtido);
     }
     
     /**
      * Propriedade 12: Se p é um número primo mdc(p,a) = p ou mdc(p,a) = 1
      */
     @Test
-    public void testMdc_P12_ResultadoPrimo() {
-        int p = 11;
-        int a = p;
-        int result = MathUtil.mdc(p, a);
-        int expResult = p;
-        assertEquals(expResult, result);
+    void testMdc_P12_obtidoadoPrimo() {
+        final int p = 11;
+        final int a = p;
+        final int esperado = p;
+        final int obtido = MathUtil.mdc(p, a);
+        assertEquals(esperado, obtido);
     }    
     
     /**
      * Propriedade 12: Se p é um número primo mdc(p,a) = p ou mdc(p,a) = 1
      */
     @Test
-    public void testMdc_P12_ResultadoIgual1() {
-        int p = 11;
-        int a = 2;
-        int result = MathUtil.mdc(p, a);
-        int expResult = 1;
-        assertEquals(expResult, result);
+    void testMdc_P12_obtidoadoIgual1() {
+        final int p = 11;
+        final int a = 2;
+        final int esperado = 1;
+        final int obtido = MathUtil.mdc(p, a);
+        assertEquals(esperado, obtido);
     }
 
     /**
@@ -194,25 +182,36 @@ public class MathUtilMdcTest {
      * do MDC. O caso geral é a regra, os outros são casos alternativos (exceções).
      */
     @Test
-    public void testMdcParaCasoGeral() {
-        int a = 30;
-        int b = 12;
-        int result = MathUtil.mdc(a, b);
-        int expResult = 6;
-        assertEquals(expResult, result);
+    void testMdcParaCasoGeral() {
+        final int a = 30;
+        final int b = 12;
+        final int esperado = 6;
+        final int obtido = MathUtil.mdc(a, b);
+        assertEquals(esperado, obtido);
     }
 
     /**
-     * Testa uma série de conjuntos de mais de 2 valores.
+     * Testa mais de dois valores.
      * Assim, a versão sobrecarregada {@link MathUtil#mdc(int...)} será
      * chamada no lugar da {@link MathUtil#mdc(int, int)}.
      */
     @Test
-    public void testMdc_Diversos() {
-        assertEquals(1, MathUtil.mdc(2, 3, 5));
-        assertEquals(2, MathUtil.mdc(2, 6, 4));
-        assertEquals(2, MathUtil.mdc(2, 12, 2, 4, 8));
-        assertEquals(3, MathUtil.mdc(3, 15, 18));
+    void testMdc_DiversosResultado1() {
+        final int esperado = 1;
+        final int obtido = MathUtil.mdc(2, 3, 5);
+        assertEquals(esperado, obtido);
+    }  
+
+    /**
+     * Testa mais de dois valores.
+     * Assim, a versão sobrecarregada {@link MathUtil#mdc(int...)} será
+     * chamada no lugar da {@link MathUtil#mdc(int, int)}.
+     */
+    @Test
+    void testMdc_DiversosResultadoMaior1() {
+        final int esperado = 2;
+        final int obtido = MathUtil.mdc(4, 2, 8);
+        assertEquals(esperado, obtido);
     }  
 
 }
--- projects/01-introducao-tdd/src/test/java/com/manoelcampos/tdd/MathUtilPrimoTest.java
+++ projects/01-introducao-tdd/src/test/java/com/manoelcampos/tdd/MathUtilPrimoTest.java
@@ -10,50 +10,50 @@ import static org.junit.jupiter.api.Assertions.*;
  *
  * @author Manoel Campos da Silva Filho
  */
-public class MathUtilPrimoTest {
+class MathUtilPrimoTest {
 
     @Test
-    public void isPrimo0() {
+    void isPrimo0() {
         assertFalse(MathUtil.isPrimo(0));
     }
 
     @Test
-    public void isPrimo1() {
+    void isPrimo1() {
         assertFalse(MathUtil.isPrimo(1));
     }
 
     @Test
-    public void isPrimo2() {
+    void isPrimo2() {
         assertTrue(MathUtil.isPrimo(2));
     }
 
     @Test
-    public void isPrimo3() {
+    void isPrimo3() {
         assertTrue(MathUtil.isPrimo(3));
     }
 
     @Test
-    public void isPrimo4() {
+    void isPrimo4() {
         assertFalse(MathUtil.isPrimo(4));
     }
 
     @Test
-    public void isPrimo5() {
+    void isPrimo5() {
         assertTrue(MathUtil.isPrimo(5));
     }
 
     @Test
-    public void isPrimo6() {
+    void isPrimo6() {
         assertFalse(MathUtil.isPrimo(6));
     }
 
     @Test
-    public void isPrimo13() {
+    void isPrimo13() {
         assertTrue(MathUtil.isPrimo(13));
     }
 
     @Test
-    public void isPrimoMaxInt() {
+    void isPrimoMaxInt() {
         assertTrue(MathUtil.isPrimo(Integer.MAX_VALUE));
     }
 }
