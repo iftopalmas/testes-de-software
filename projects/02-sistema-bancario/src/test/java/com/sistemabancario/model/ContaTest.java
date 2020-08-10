6977a239de7b1be8668720e42f760e3134cf7af2
Manoel Campos <manoelcampos@gmail.com>
2020-08-10 16:36:30 -0300

    Adiciona ContaTest vazia no Sistema Bancário como exemplo

---
+++ projects/02-sistema-bancario/src/test/java/com/sistemabancario/model/ContaTest.java
@@ -0,0 +1,13 @@
+package com.sistemabancario.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContaTest {
+    
+    @Test
+    public void testAddMovimentacao() {
+        //TODO: Você precisa apagar estas duas linhas e implementar o teste
+        fail("Teste não implementado");
+    }
+}
