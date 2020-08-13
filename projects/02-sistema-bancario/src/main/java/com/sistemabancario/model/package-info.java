f80fdd8bae2fb7afbf251f30225a635d2c2264aa
Manoel Campos <manoelcampos@gmail.com>
2020-08-13 10:02:27 -0300

    Remove sistema bancário para inclusão como sub-módulo

--- projects/02-sistema-bancario/src/main/java/com/sistemabancario/model/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Pacote contendo as classes de negócio do sistema, ou seja,
- * as classes principais que importam para o negócio do cliente (dono do sistema).
- *
- * @author Manoel Campos da Silva Filho
- */
-package com.sistemabancario.model;
